8dcac9b69308e1651d8e319f0b732b7a7941aad0
Александр Задорожный <zadorozhniy76@gmail.com>
2024-10-15 17:32:19 +0300

    homework

---
+++ main.py
@@ -0,0 +1,45 @@
+print('Hello world') # This is a sample Python script.
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# print (12),
+# print (type(12))
+# print(12*12)
+# print(12%12)
+# print (12/12) # /- простое деление
+# print(12//12) # //-целочисленное деление
+# print(12**3) # ** -возведение в степень
+# print (5.3)
+# print (type(5.3))
+# print (type (5.3+12))
+# print ("Hello, Alex") #-слова в строке всегда!нач инаются с кавычек (одинарных или двойных)
+
+a = 12
+b = 89
+c = 1
+print( a, '+', b, '-', c)
+
+# 1 st program
+a = 9
+b = 5
+print((a*b)**0,5)
+# 2 nd program
+a = 9.99>9,98
+b = 1000<1000.1
+print(a and b )
+# 3 rd program
+a = (2*2+2)
+print(a)
+b = (2*(2+2))
+print(b)
+print(a==b)
+# 4th program
+str_='123.456'
+str_2 = float(str_)
+#print((type)(str_2))
+str_2=str_2*10
+#print(str_2)
+str_3=int(str_2)
+#print(str_3)
+print(str_3%10)
